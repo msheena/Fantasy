4e75fd0febba286e0db7653c547b542aa45c89d0
sheena <msheena@163.com>
2019-12-31 15:00:24 +0800

    recycleview悬浮item demo

--- app/src/main/java/cn/tail/fantasy/MainActivity.kt
+++ app/src/main/java/cn/tail/fantasy/MainActivity.kt
@@ -1,35 +1,67 @@
 package cn.tail.fantasy
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cn.tail.fantasy.material.SuspensionAdapter
 import cn.tail.fantasy.pop.AnglePopUpWindow
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
-
+    var height=0
+    var currentPos=0
+    lateinit var suspensionTitle:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        var testLayout=findViewById<AngelLayout>(R.id.test_angel)
-//        testLayout.setAnchorView()
-        var btnTest=findViewById<Button>(R.id.test_button)
-        val function: (View) -> Unit = {
-            val view=LayoutInflater.from(this).inflate(R.layout.test_menu, null);
-            val testPop = AnglePopUpWindow(this, 300, 300)
-            testPop.inflateView(view);
-            testPop.setAngleColor(Color.CYAN)
-            testPop.setAngleDimensions(15f,9f)
-            testPop.showAsDropDown(btnTest)
-//            testPop.showAtLocation(btnTest,Gravity.TOP,0,0)
-        }
-        btnTest.setOnClickListener(function)
+        suspensionTitle=findViewById<TextView>(R.id.tv_suspension_bar)
+        val recyclerView=findViewById<RecyclerView>(R.id.recyclerview)
+        val layoutManager=LinearLayoutManager(this)
+        recyclerView.layoutManager=layoutManager
+        val adapter=SuspensionAdapter()
+        recyclerView.adapter=adapter
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                height=suspensionTitle.height
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val view=layoutManager.findViewByPosition(currentPos+1)
+                if (view!=null){
+                    if(view.top<=height){
+                        suspensionTitle.y= (-(height-view.top)).toFloat()
+                    }else{
+                        suspensionTitle.y=0f
+                    }
+                }
+
+                if (currentPos!=layoutManager.findFirstVisibleItemPosition()){
+                    currentPos=layoutManager.findFirstVisibleItemPosition()
+                    updateSuspensionBar()
+                }
+
+
+            }
+        })
+
+
     }
 
-//    @ClickBehavior("click")
-//    public fun click(){
-//
-//    }
+    fun updateSuspensionBar(){
+        suspensionTitle.setText("item: $currentPos")
+    }
+
+
 }
